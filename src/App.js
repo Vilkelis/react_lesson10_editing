9f4c177af72cd3cdd9cd308105beb889756b62bb
Stepan Golovanov <vilkelis@yandex.ru>
2020-05-16 21:18:29 +0500

    redux crud

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ServiceForm from './components/ServiceForm';
+import ServiceList from './components/ServiceList';
+import ServiceFormClassBased from './components/ServiceFormClassBased';
+import ServiceListClassBased from './components/ServiceListClassBased';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ServiceForm />
+      <ServiceList />
+      <hr />
+      <ServiceFormClassBased />
+      <ServiceListClassBased />
+    </>
   );
 }
 
